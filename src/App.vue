fd25b2417506f61b2c1ebaa1b3f18a7c928ed39d
leemove <923522454@qq.com>
2017-08-28 17:53:31 +0800

    init 调通ajax

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <mt-header fixed title="有盐"></mt-header>
+    <router-view></router-view>
+    <mt-button type="primary" size="small" @click="test">测试按钮</mt-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  methods: {
+    test () {
+      this.$http.post('/user/sign')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
